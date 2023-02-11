eacee1675c19939ad9c66a29c638cbe6136314d6
Kwasi Asiedu <createdarko@gmail.com>
2023-02-11 12:52:41 +0000

    task completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BpkCode } from '@skyscanner/backpack-web/bpk-component-code';
 import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
 import BpkText from '@skyscanner/backpack-web/bpk-component-text';
+import BpkCalendar from '@skyscanner/backpack-web/bpk-component-calendar';
 
 import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
 
@@ -9,20 +10,34 @@ import STYLES from './App.scss';
 
 const getClassName = cssModules(STYLES);
 
-const App = () => (
-  <div className={getClassName('App')}>
-    <header className={getClassName('App__header')}>
-      <div className={getClassName('App__header-inner')}>
-        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Welcome to React + Backpack</BpkText>
-      </div>
-    </header>
-    <main className={getClassName('App__main')}>
-      <BpkText tagName="p" className={getClassName('App__text')}>
-        To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
-      </BpkText>
-      <BpkButton onClick={() => alert('It works!')}>Click me</BpkButton>
-    </main>
-  </div>
-);
+
+const App = () => {
+
+
+
+  return (
+    <div className={getClassName('App')}>
+      <header className={getClassName('App__header')}>
+        <div className={getClassName('App__header-inner')}>
+          <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Flight Schedule</BpkText>
+        </div>
+      </header>
+      <main className={getClassName('App__main')}>
+        <BpkText tagName="p" className={getClassName('App__text')}>
+          To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
+        </BpkText>
+        <BpkCalendar
+          id='calendar'
+          weekStartsOn={1}
+          changeMonthLabel="Change month"
+          nextMonthLabel="Next month"
+          previousMonthLabel="Previous month"
+        />
+        <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
+      </main>
+    </div>
+  );
+
+}
 
 export default App;
